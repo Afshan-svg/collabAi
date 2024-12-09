991e200d97febf6f776564a199ef900b28cfef4e
Afshan Khan <70753651+Afshan-svg@users.noreply.github.com>
2024-12-09 16:15:44 +0530

    Update collab.js

--- collab.js
+++ collab.js
@@ -18,39 +18,43 @@
           <div style="display:flex;justify-content:space-between;align-items:center;padding:10px;background: linear-gradient(90deg, #1969E9 0%, #05B8FB 100%);border-top-left-radius:10px;border-top-right-radius:10px;">
               <h4 style="margin:0;color:white;font-size:16px;">${assistantName} Assistant</h4>
               <button id="minimize-button" style="border:none;background:transparent;cursor:pointer;font-size:20px;">
-                <i class="fas fa-chevron-down" style="color:#ffffff;"></i>
+                <i class="fas fa-chevron-dwn" style="color:#ffffff;"></i>
               </button>
           </div>
           <iframe id="chatbot-iframe" width="100%" height="100%"
               style="border: none; border-radius: 0 0 10px 10px;" title="Custom Chatbot"></iframe>
         </div>
       </div>
-      <style>
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
-        @keyframes bounce {
-          0%, 20%, 50%, 80%, 100% {transform:translateY(0);}
-          40% {transform:translateY(-10px);}
-          60% {transform:translateY(-5px);}
-        }
-        #chatbot-icon {
-          width: 150px;
-          height: 150px;
-        }
-        #chatbot-icon img {
-          width: 130px;
-          height: 130px;
-          object-fit: cover;
-          border-radius: 0;
-        }
-        
-        .container-fluid.CustomFlex {
-        display: none !important;
-        }
-      </style>
+     <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  @keyframes bounce {
+    0%, 20%, 50%, 80%, 100% {transform:translateY(0);}
+    40% {transform:translateY(-10px);}
+    60% {transform:translateY(-5px);}
+  }
+
+  #chatbot-icon {
+    width: 150px;
+    height: 150px;
+  }
+
+  #chatbot-icon img {
+    width: 130px;
+    height: 130px;
+    object-fit: cover;
+    border-radius: 0;
+  }
+  
+  .container-fluid.CustomFlex {
+    display: none !important;
+  }
+</style>
+
   `;
   document.body.appendChild(container);
 
