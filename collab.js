d5b1fcbd684a23ac8492f6ee1f68c886b672c705
Afshan Khan <70753651+Afshan-svg@users.noreply.github.com>
2024-12-09 16:59:44 +0530

    Update collab.js

--- collab.js
+++ collab.js
@@ -60,7 +60,7 @@
   document.body.appendChild(container);
 
   if (assistantName && assistantId) {
-      document.getElementById("chatbot-iframe").src = `https://collabai.buildyourai.consulting/${assistantId}?embedded=true`;
+      document.getElementById("chatbot-iframe").src = `http://localhost:3000/agents/${assistantId}?embedded=true`;
       console.log(`Iframe src set to: https://collabai.buildyourai.consulting/${assistantId}`);
   } else {
       console.error("Assistant name or ID not provided.");
