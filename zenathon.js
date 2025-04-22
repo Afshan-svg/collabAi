32e700c9bda2e2ea3ae1005096b6f2eb6a3be6d6
Afshan Khan <70753651+Afshan-svg@users.noreply.github.com>
2025-04-23 00:41:03 +0530

    https://afshan-svg.github.io/collabAi/embedbot.png

--- zenathon.js
+++ zenathon.js
@@ -14,7 +14,7 @@
   container.innerHTML = `
       <div id="assistant-embed-container">
         <div id="chatbot-icon" style="position:fixed;bottom:40px;right:50px;width:120px;height:120px;display:flex;align-items:center;justify-content:center;cursor:pointer;animation:bounce 2s infinite;z-index:9999;">
-          <img src="https://afshan-svg.github.io/collabAi/roboooo.jpg" alt="Chatbot" style="width:150px;height:120px;object-fit:contain;" />
+          <img src="https://afshan-svg.github.io/collabAi/embedbot.png" alt="Chatbot" style="width:150px;height:120px;object-fit:contain;" />
         </div>
         <div id="assistant-embed" style="position:fixed;bottom:20px;right:20px;width:720px;height:660px;border:1px solid #ccc;border-radius:10px;display:none;z-index:9999;">
           <div style="display:flex;justify-content:space-between;align-items:center;padding:10px;background: linear-gradient(90deg, #1969E9 0%, #05B8FB 100%);border-top-left-radius:10px;border-top-right-radius:10px;">
